673a086e8173b3be8994473b0f5358db1c7b525f
lukeathedev <alvarengalucas55@gmail.com>
2023-02-27 20:31:51 -0300

    Better Logging

--- src/logger.ts
+++ src/logger.ts
@@ -1,25 +1,17 @@
 import { createLogger, format, transports } from "winston";
 
 export const logger = createLogger({
-  level: 'info',
+  level: "info",
   format: format.simple(),
-  defaultMeta: { service: 'user-service' },
+  defaultMeta: { service: "user-service" },
   transports: [
-    //
-    // - Write all logs with importance level of `error` or less to `error.log`
-    // - Write all logs with importance level of `info` or less to `combined.log`
-    //
-    // new transports.File({ filename: 'error.log', level: 'error' }),
-    // new transports.File({ filename: 'combined.log' }),
+    new transports.Console({
+      format: format.combine(
+        format.timestamp(),
+        format.printf(({ level, message, timestamp }) => {
+          return `${timestamp} [${level.toUpperCase()}] :: ${message}`;
+        })
+      ),
+    }),
   ],
 });
-
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new transports.Console({
-    format: format.simple(),
-  }));
-}
