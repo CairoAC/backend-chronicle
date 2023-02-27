25055aa231b1f48493a2b6114576d2fd7b70725a
CairoAC <cairoayrescosta14@gmail.com>
2023-02-27 20:37:32 -0300

    att3

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,7 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import { db } from "./database";
-
-// App
-const app = express()
-const port = 8080
-
-const noCors = (req: Request, res: Response, next: any) => {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-}
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(noCors);
+import { app} from "./app"
 
 const apipath = "/api/v1"
 
